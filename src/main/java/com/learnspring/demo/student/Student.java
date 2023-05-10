65e18d01d4103f88d085d31eae1011a4d5c81dc4
Arsh Khatwani <arsh.khatwani@gmail.com>
2023-05-10 16:45:15 +0530

    Add jpa in student class

--- src/main/java/com/learnspring/demo/student/Student.java
+++ src/main/java/com/learnspring/demo/student/Student.java
@@ -2,15 +2,37 @@ package com.learnspring.demo.student;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table
 @Builder
 public class Student {
-    @Getter @Setter private Long id;
-    @Getter @Setter private String name;
-    @Getter @Setter private String email;
-    @Getter @Setter private LocalDate dob;
-    @Getter @Setter private Integer age;
+    @Id
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+
+    @Getter
+    @Setter
+    private Long id;
+    @Getter
+    @Setter
+    private String name;
+    @Getter
+    @Setter
+    private String email;
+    @Getter
+    @Setter
+    private LocalDate dob;
+    @Getter
+    @Setter
+    private Integer age;
 }
