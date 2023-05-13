041d6146820dd9f98936be16a8af6d3298e9ec83
Arsh Khatwani <arsh.khatwani@gmail.com>
2023-05-13 14:45:51 +0530

    Add student save feature on post request

--- src/main/java/com/learnspring/demo/student/StudentRepository.java
+++ src/main/java/com/learnspring/demo/student/StudentRepository.java
@@ -1,6 +1,9 @@
 package com.learnspring.demo.student;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 // <Student, Long> => <type of object, type of id>
@@ -8,4 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
